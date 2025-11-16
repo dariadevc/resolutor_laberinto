cd208e3f1c1d55a580de5ba4ab1aaeabaed7eb7b
Daria <dsauco36@gmail.com>
2025-11-16 18:41:26 -0300

    terminé de armar algoritmo que reconoce la tile correcta

--- camino/camino.py
+++ camino/camino.py
@@ -1,134 +1,151 @@
 class Camino:
-    
+
     def __init__(self, static_grid, tamanio_celda, tile_images):
-        
+
         self.grid = [fila[:] for fila in static_grid]
-        self.n = len(self.grid) 
+        self.n = len(self.grid)
         self.tamanio_celda = tamanio_celda
         self.tile_images = tile_images
 
     def get_celda(self, fila, col):
         if 0 <= fila < self.n and 0 <= col < self.n:
             return self.grid[fila][col]
-        return '1' 
+        return '1'
 
     def set_celda(self, fila, col, value):
         if 0 <= fila < self.n and 0 <= col < self.n:
             self.grid[fila][col] = value
 
-    # La idea es que se reconozca qué tile poner según ls vecinos que tiene!!
-    # def get_tile_type(self, fila, col):
-    #     """Determina el tipo de tile basado en los vecinos"""
-    #     if self.grid[fila][col] != '1':  # No es pared
-    #         return self.grid[fila][col]
+    # verifica si la celda es una pared
+    def es_pared(self, fila, col):
+        if 0 <= fila < self.n and 0 <= col < self.n:
+            return self.grid[fila][col] == '1'
+        return False
+
+    # verifica si la celda es un camino
+    def es_camino(self, fila, col):
+        if 0 <= fila < self.n and 0 <= col < self.n:
+            return self.grid[fila][col] in ['0', 'V', 'E']
+        return False
+
+    def obtener_tipo_tile(self, fila, col):
+
+        # si no es una pared, usa la tile camino.png
+        if not self.es_pared(fila, col):
+            return self.get_celda(fila, col)
+
+        # Verificar los 8 vecinos, B2 representa la celda actual
+
+        # A1 | A2 | A3
+        # B1 | B2 | B3
+        # C1| C2 | C3
+
+        # Y una celda extra por debajo de C2 para las paredes
+
+        # D2
+
+        # primer fila
+        a1 = self.es_pared(fila - 1, col - 1) if 0 <= fila-1 < self.n and 0 <= col-1 < self.n else True
+        a2 = self.es_pared(fila - 1, col) if 0 <= fila-1 < self.n and 0 <= col < self.n else True
+        a3 = self.es_pared(fila - 1, col + 1) if 0 <= fila-1 < self.n and 0 <= col+1 < self.n else True
+
+        #segunda fila
+        b1 = self.es_pared(fila, col - 1) if 0 <= fila < self.n and 0 <= col-1 < self.n else True
+        b2 = self.es_pared(fila, col) if 0 <= fila < self.n and 0 <= col < self.n else True
+        b3 = self.es_pared(fila, col + 1) if 0 <= fila < self.n and 0 <= col+1 < self.n else True
+
+        #tercer fila
+        c1 = self.es_pared(fila + 1, col - 1) if 0 <= fila+1 < self.n and 0 <= col-1 < self.n else True
+        c2 = self.es_pared(fila + 1, col) if 0 <= fila+1 < self.n and 0 <= col < self.n else True
+        c3 = self.es_pared(fila + 1, col + 1) if 0 <= fila+1 < self.n and 0 <= col+1 < self.n else True
+
+        #cuarta fila
+        d2 = self.es_pared(fila + 2, col) if 0 <= fila+2 < self.n and 0 <= col < self.n else True
+
+        # ==== Determinar el tipo de tile basado en los vecinos ====
+
+        # --- CELDAS CENTRALES ---
+
+        # Costado izquierdo
+        if a2 and a3 and b3 and c2 and c3 and ((not b1 and d2)or (b1 and not c1 and d2)):
+            return 'CIZ'
+
+        # Centro
+        if a1 and a2 and a3 and b1 and b3 and c1 and c2 and c3 and d2:
+            return 'CEN'
         
-    #     # Sistema de bits para determinar el tipo de pared
-    #     bitmask = 0
+        # Costado derecho
+        if a1 and a2 and b1 and c1 and c2 and ((not b3 and d2) or (b3 and not c3 and d2)):
+            return 'CDE'
         
-    #     # Verificar los 8 vecinos (solo nos interesan las paredes '1')
-    #     # Arriba-izquierda (1)
-    #     if fila > 0 and col > 0 and self.grid[fila-1][col-1] == '1':
-    #         bitmask |= 1
-    #     # Arriba (2)
-    #     if fila > 0 and self.grid[fila-1][col] == '1':
-    #         bitmask |= 2
-    #     # Arriba-derecha (4)
-    #     if fila > 0 and col < self.n-1 and self.grid[fila-1][col+1] == '1':
-    #         bitmask |= 4
-    #     # Izquierda (8)
-    #     if col > 0 and self.grid[fila][col-1] == '1':
-    #         bitmask |= 8
-    #     # Derecha (16)
-    #     if col < self.n-1 and self.grid[fila][col+1] == '1':
-    #         bitmask |= 16
-    #     # Abajo-izquierda (32)
-    #     if fila < self.n-1 and col > 0 and self.grid[fila+1][col-1] == '1':
-    #         bitmask |= 32
-    #     # Abajo (64)
-    #     if fila < self.n-1 and self.grid[fila+1][col] == '1':
-    #         bitmask |= 64
-    #     # Abajo-derecha (128)
-    #     if fila < self.n-1 and col < self.n-1 and self.grid[fila+1][col+1] == '1':
-    #         bitmask |= 128
-            
-    #     return self.bitmask_to_tile_type(bitmask)
+        # --- CELDAS SUPERIORES ---
+
+        # Esquina superior izquierda
+        if not a1 and not a2 and not b1 and b3 and c2 and c3 and d2:
+            return 'ESI'
     
-    # def bitmask_to_tile_type(self, bitmask):
-    #     """Convierte la máscara de bits a un tipo de tile específico"""
-    #     # Definir los tipos básicos según la conectividad
-    #     top = (bitmask & 2) != 0
-    #     bottom = (bitmask & 64) != 0
-    #     left = (bitmask & 8) != 0
-    #     right = (bitmask & 16) != 0
+        # Centro superior
+        if not a2 and b1 and b3 and c1 and c2 and c3 and d2:
+            return 'CSU'
         
-    #     # Paredes simples
-    #     if top and bottom and left and right:
-    #         return 'wall_center'  # Pared completamente rodeada
+        # Esquina superior derecha
+        if not a2 and not a3 and not b3 and b1 and c1 and c2 and d2:
+            return 'ESD'
         
-    #     # Esquinas
-    #     if top and right and not left and not bottom:
-    #         return 'wall_corner_top_left'
-    #     if top and left and not right and not bottom:
-    #         return 'wall_corner_top_right'
-    #     if bottom and right and not left and not top:
-    #         return 'wall_corner_bottom_left'
-    #     if bottom and left and not right and not top:
-    #         return 'wall_corner_bottom_right'
-            
-    #     # # Paredes verticales
-    #     # if top and bottom and not left and not right:
-    #     #     return 'wall_vertical'
-            
-    #     # Paredes horizontales
-    #     if left and right and not top and not bottom:
-    #         return 'wall_horizontal'
-            
-    #     # # T-connections
-    #     # if top and left and right and not bottom:
-    #     #     return 'wall_t_down'
-    #     # if bottom and left and right and not top:
-    #     #     return 'wall_t_up'
-    #     # if left and top and bottom and not right:
-    #     #     return 'wall_t_right'
-    #     # if right and top and bottom and not left:
-    #     #     return 'wall_t_left'
-            
-    #     # Paredes con extremos
-    #     if top and not bottom and not left and not right:
-    #         return 'wall_end_bottom'
-    #     if bottom and not top and not left and not right:
-    #         return 'wall_end_top'
-    #     if left and not right and not top and not bottom:
-    #         return 'wall_end_right'
-    #     if right and not left and not top and not bottom:
-    #         return 'wall_end_left'
-            
-    #     return 'wall_single'  # Pared aislada
+        # -- CELDAS INFERIORES ---
 
+        # Esquina inferior izquierda
+        if a2 and a3 and b3 and c2 and c3 and ((a1 and b1) or (not a1 and not b1) or (a1 and not b1)) and not c1 and not d2:
+            return 'EII'
+        
+        # Centro inferior
+        if a2 and b1 and b3 and c2 and c3 and not d2:
+            return 'CIN'
+        
+        # Esquina inferior derecha
+        if a1 and a2 and b1 and c1 and c2 and ((a3 and b3) or (not a3 and not b3) or (a3 and not b3)) and not c3 and not d2:
+            return 'EID'
 
-    # def dibujar(self, pantalla, pygame_instance, offset_x=0, offset_y=0):
-    #     for fila in range(self.n):
-    #         for col in range(self.n):
-    #             # Obtener el tipo de tile basado en los vecinos
-    #             tile_type = self.get_tile_type(fila, col)
-    #             imagen_a_dibujar = self.tile_images.get(tile_type)
-                
-    #             pos_x = col * self.tamanio_celda + offset_x
-    #             pos_y = fila * self.tamanio_celda + offset_y
-                
-    #             if imagen_a_dibujar:
-    #                 pantalla.blit(imagen_a_dibujar, (pos_x, pos_y))
+        # -- CELDAS SUP E INF ---
+
+        # Esquina superior e inferior izquierda
+        if not a1 and not a2 and not b1 and not c1 and not d2 and b3 and c2 and c3:
+            return 'ESII'
+
+        # Centro superior e inferior
+        if not a2 and b1 and b3 and c1 and c2 and c3 and not d2:
+            return 'CSI'
+        
+        # Esquina superior e inferior derecha
+        if not a2 and not a3 and not b3 and not c3 and not d2 and b1 and c1 and c2:
+            return 'ESDI'
+        
+        # -- PAREDES ---
+
+        # Pared izquierda
+        if a2 and a3 and b3 and not b1 and not c1 and not c2:
+            return 'PI'
+        
+        # Pared central
+        if a1 and a2 and a3 and b1 and b3 and not c2:
+            return 'PC'
+        
+        # Pared derecha
+        if a1 and a2 and b1 and not b3 and not c2 and not c3:
+            return 'PD'
+        
+        return 'CEN'  # Valor por defecto si no coincide con ningún patrón
 
 
     def dibujar(self, pantalla, pygame_instance, offset_x=0, offset_y=0):
         for fila in range(self.n):
             for col in range(self.n):
-                char_key = self.grid[fila][col]
-                imagen_a_dibujar = self.tile_images.get(char_key)
-                
+                tipo_tile = self.obtener_tipo_tile(fila, col)
+                imagen_a_dibujar = self.tile_images.get(tipo_tile)
+
                 pos_x = col * self.tamanio_celda + offset_x
                 pos_y = fila * self.tamanio_celda + offset_y
-                
+
                 if imagen_a_dibujar:
                     pantalla.blit(imagen_a_dibujar, (pos_x, pos_y))
 
@@ -138,6 +155,6 @@ class Camino:
             # Convertir fila a string y reemplazar 'E' por '0'
             fila_str = ''.join(fila).replace('E', '0')
             filas_transformadas.append(fila_str)
-        
+
         # Unir todas las filas con '#' entre ellas
         return '#'.join(filas_transformadas)
